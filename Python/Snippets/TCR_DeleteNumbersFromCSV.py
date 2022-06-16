4504f739060ebd47bf6f55341e3616798fbbbc2b
Cassie <bowles.cassiee@gmail.com>
2022-06-16 14:19:23 -0500

    new TCR script for COPS

---
+++ Python/Snippets/TCR_DeleteNumbersFromCSV.py
@@ -0,0 +1,54 @@
+from requests.auth import HTTPBasicAuth
+import requests
+import csv
+
+# assign auth variables to be used later
+SpaceURL = '.signalwire.com'
+projectID = ""
+authToken = ""
+campaignID = ""
+host = f"https://{SpaceURL}"
+results = []
+
+# read in CSV of numbers to delete
+with open("Numbers.csv", 'r', encoding='utf-8-sig') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+
+    for row in reader:
+        if "+" not in row[0]:
+            results.append("+" + row[0])
+        else:
+            results.append(row[0])
+
+# List All Campaign Numbers
+url = f"https://{SpaceURL}/api/relay/rest/registry/beta/campaigns/{campaignID}/numbers?page_size=1000"
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload, auth=HTTPBasicAuth(projectID, authToken)).json()
+campaignNumbers = response['data']
+
+while "next" in response['links'].keys():
+    response = requests.get(host + response['links']['next'], auth=HTTPBasicAuth(projectID, authToken)).json()
+    campaignNumbers.extend(response['data'])
+
+# loop through numbers and call delete number function on each if tn in results
+for number in campaignNumbers:
+    tn = number['phone_number']['number']
+    assignmentSID = number['id']
+    if tn in results:
+
+        try:
+            url = f"https://{SpaceURL}/api/relay/rest/registry/beta/numbers/{assignmentSID}"
+            payload = {}
+            headers = {}
+            response = requests.request("DELETE", url, headers=headers, data=payload,
+                                        auth=HTTPBasicAuth(projectID, authToken))
+            if response.ok:
+                print(f"{tn} successfully deleted")
+            else:
+                print(f"{response.status_code} {response.text}. {tn} not deleted.")
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
